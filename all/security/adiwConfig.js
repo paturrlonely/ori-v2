8246e1f9124b2f84818b67003fb13f99fad47ccf
paturrlonely <boboiapi54321@gmail.com>
2024-11-27 00:22:36 +0700

    Update adiwConfig.js

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '6281260431003',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
   },
-  'creatorScript': '6289508082845',
+  'creatorScript': '6281260431003',
   'filePath': "./all/approval",
   'checkFilePath': "../setting/osaragi.js",
   'codeToDetect': 'main();'
